180563e8f60b05665530a1c0ff5eb200375634ff
Renan Martins <renanmartins1999@hotmail.com>
2026-03-08 14:40:10 -0300

    feat: add edit page

---
+++ scripts/project-create-edit.js
@@ -0,0 +1,15 @@
+window.onload = function() {
+    const screenType = 'create';
+
+    if(screenType == 'create')
+    {
+        document.querySelector('#main-title').innerText = "Vamos cadastrar seu novo projeto!";
+        document.querySelector('#action-button').innerText = "Cadastrar";
+    }
+
+    if(screenType == 'edit')
+    {
+        document.querySelector('#main-title').innerText = "Editar projeto";
+        document.querySelector('#action-button').innerText = "Salvar";
+    }
+}
