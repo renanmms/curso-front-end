4b23ffc3dafe897ff21920a896a49d571d08a84c
Renan Martins <renanmartins1999@hotmail.com>
2026-03-09 19:46:43 -0300

    feat: check if role is selected

--- scripts/login.js
+++ scripts/login.js
@@ -1,4 +1,32 @@
 function cadastrar()
 {
-    console.log('User created!');
+    if(!checkIfAnyRoleIsChecked())
+    {
+        alert("Please select a role!");
+    }
+
+    let payload = {
+        fullName: document.querySelector('#fullName').value,
+        birthDate: document.querySelector('#birthDate').value,
+        email: document.querySelector('#email').value,
+        password: document.querySelector('#password').value,
+        role: ''
+    }    
+}
+
+
+function checkIfAnyRoleIsChecked() 
+{
+    let counter = 0;
+    let list = document.getElementsByName("role");
+
+    for(let radioButton of list) {
+        if(radioButton.checked === false) {
+            counter++;
+        }   
+    }
+
+    let result = counter !== list.length;
+
+    return result;
 }
