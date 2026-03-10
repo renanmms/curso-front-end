f6ddb4498e5cbab0c6630f3c7c4942d21e749791
Renan Martins <renanmartins1999@hotmail.com>
2026-03-09 22:10:27 -0300

    feat: post user to mock api

--- scripts/login.js
+++ scripts/login.js
@@ -1,24 +1,43 @@
-function cadastrar()
-{
-    if(!checkIfAnyRoleIsChecked())
-    {
-        alert("Please select a role!");
-    }
+const Role = {
+    Dev: 'Dev',
+    Client: 'Client'
+};
 
+function cadastrar() {
+    if(!checkIfAnyRoleIsChecked()) {
+        alert('Please select a role!');
+        return;
+    }
+    
     let payload = {
         fullName: document.querySelector('#fullName').value,
         birthDate: document.querySelector('#birthDate').value,
         email: document.querySelector('#email').value,
         password: document.querySelector('#password').value,
-        role: ''
-    }    
+        role: getSelectedRole()
+    };
+
+    fetch('https://69adb822b50a169ec88017c7.mockapi.io/api/users', {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: {
+            'Content-Type': 'application/json'
+        }   
+    })
+    .then(response => response.json())
+    .then(response => {
+        alert('Created successfully!');
+    })
+    .catch(error => {
+        alert('Internal Server Error');
+        console.log(error);
+    });
 }
 
 
-function checkIfAnyRoleIsChecked() 
-{
+function checkIfAnyRoleIsChecked() {
     let counter = 0;
-    let list = document.getElementsByName("role");
+    let list = document.getElementsByName('role');
 
     for(let radioButton of list) {
         if(radioButton.checked === false) {
@@ -29,4 +48,8 @@ function checkIfAnyRoleIsChecked()
     let result = counter !== list.length;
 
     return result;
+}
+
+function getSelectedRole() {
+    return document.getElementsByName('role')[0].checked === true ? Role.Dev : Role.Client;
 }
