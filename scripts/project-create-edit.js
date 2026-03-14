7ce7a9c3492f16151d1cd274e5e9fc7a2df0dc59
Renan Martins <renanmartins1999@hotmail.com>
2026-03-13 21:44:12 -0300

    feat: post project to mock api

--- scripts/project-create-edit.js
+++ scripts/project-create-edit.js
@@ -6,10 +6,37 @@ window.onload = function() {
         document.querySelector('#main-title').innerText = "Vamos cadastrar seu novo projeto!";
         document.querySelector('#action-button').innerText = "Cadastrar";
     }
+    
+    // if(screenType == 'edit')
+    // {
+    //     document.querySelector('#main-title').innerText = "Editar projeto";
+    //     document.querySelector('#action-button').innerText = "Salvar";
+    // }
+}
 
-    if(screenType == 'edit')
-    {
-        document.querySelector('#main-title').innerText = "Editar projeto";
-        document.querySelector('#action-button').innerText = "Salvar";
-    }
+
+function cadastrar()
+{
+
+    let payload = {
+        title: document.querySelector('#title').value,
+        totalCost: Number(document.querySelector('#total-cost').value),
+        description: document.querySelector('#description').value
+    };
+
+    fetch('https://69adb822b50a169ec88017c7.mockapi.io/api/projects', {
+        method: 'POST',
+        body: JSON.stringify(payload),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+    .then(response => response.json())
+    .then(response => {
+        alert('Project created successfully!');
+    })
+    .catch(error => {
+        alert('Internal Server Error');
+        console.log(error);
+    });
 }
