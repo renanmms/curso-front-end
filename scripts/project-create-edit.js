150975110459dced13fc23821dec723cf1266707
Renan Martins <renanmartins1999@hotmail.com>
2026-03-14 16:25:27 -0300

    feat: add validation before post

--- scripts/project-create-edit.js
+++ scripts/project-create-edit.js
@@ -17,12 +17,16 @@ window.onload = function() {
 
 function cadastrar()
 {
-
-    let payload = {
-        title: document.querySelector('#title').value,
-        totalCost: Number(document.querySelector('#total-cost').value),
-        description: document.querySelector('#description').value
+    const payload = {
+        title: document.querySelector('#title').value.trim(),
+        totalCost: Number(document.querySelector('#total-cost').value.trim()),
+        description: document.querySelector('#description').value.trim()
     };
+
+    if(!isValid(payload)){
+        alert('Project is not valid!');
+        return;
+    }
 
     fetch('https://69adb822b50a169ec88017c7.mockapi.io/api/projects', {
         method: 'POST',
@@ -39,4 +43,12 @@ function cadastrar()
         alert('Internal Server Error');
         console.log(error);
     });
+}
+
+function isValid(payload) {
+    const titleIsValid = payload.title !== '';
+    const descriptionIsValid = payload.description !== '';
+    const totalCostIsValid = Number.isFinite(payload.totalCost);
+
+    return titleIsValid && totalCostIsValid && descriptionIsValid;
 }
