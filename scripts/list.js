60892d5440c1a187c5d31d9309f64fc370b25180
Renan Martins <renanmartins1999@hotmail.com>
2026-03-21 17:05:43 -0300

    feat: delete project and update table

--- scripts/list.js
+++ scripts/list.js
@@ -1,7 +1,9 @@
-getProjects();
+let list = [];
 
-document.querySelector('#name').innerText = localStorage.getItem('username');
-document.querySelector('#role').innerText = localStorage.getItem('role');
+document.querySelector("#name").innerText = localStorage.getItem("userName");
+document.querySelector("#role").innerText = localStorage.getItem("role");
+
+getProjects();
 
 function redirectToCreateProject() {
     window.location.href = 'project-create-edit.html';
@@ -11,26 +13,51 @@ function getProjects() {
     fetch('https://69adb822b50a169ec88017c7.mockapi.io/api/projects')
     .then(response => response.json())
     .then(response => {
-        let template = ''
-        response.forEach(element => {
-            template = `<div class="row">
-                            <div class="title-description">
-                                <h6 class="title">${element.title}</h6>
-                                <p class="description">${element.description}</p>
-                                </div>
-                                <div class="total-cost">R$ ${element.totalCost}</div>
-                                <div class="actions">
-                                <span class="material-icons edit">
-                                    edit
-                                </span> 
-                                <span class="material-icons delete">
-                                    delete
-                                </span>
-                            </div>
-                        </div>`
+        list = response;
+        buildTable();
+    });
+}
 
-            document.querySelector('#table-body').insertAdjacentHTML('beforeend', template);
-        });
-        console.log(response);
+function goToEdit(id) {
+    window.location.href = `project-create-edit.html?id=${id}`;
+}
+
+function deleteProject(id) {
+    fetch(`https://69adb822b50a169ec88017c7.mockapi.io/api/projects/${id}`, {
+        method: 'DELETE'
+    })
+    .then(response => response.json())
+    .then(response => {
+        list = list.filter(project => project.id !== String(id));
+
+        buildTable();
+    });
+}
+
+function buildTable() {
+    console.log('cleaning table body...');
+    document.querySelector('#table-body').innerHTML = '';
+
+
+    console.log('building table...');
+    list.forEach(element => {
+        let template = 
+                    `<div class="row">
+                        <div class="title-description">
+                            <h6 class="title">${element.title}</h6>
+                            <p class="description">${element.description}</p>
+                            </div>
+                            <div class="total-cost">R$ ${element.totalCost}</div>
+                            <div class="actions">
+                            <span class="material-icons edit" onclick="goToEdit(${element.id})">
+                                edit
+                            </span> 
+                            <span class="material-icons delete" onclick="deleteProject(${element.id})">
+                                delete
+                            </span>
+                        </div>
+                    </div>`
+
+        document.querySelector('#table-body').insertAdjacentHTML('beforeend', template);
     });
 }
