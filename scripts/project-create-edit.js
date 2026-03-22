026ebadde8f58751188ba2cb638a8114d773e5e3
Renan Martins <renanmartins1999@hotmail.com>
2026-03-22 16:08:51 -0300

    feat: get and validate idClient on create project

--- scripts/project-create-edit.js
+++ scripts/project-create-edit.js
@@ -8,55 +8,54 @@ const params = Object.fromEntries(urlSearchParams.entries());
 
 let screenType = params.id ? ScreenType.Edit : ScreenType.Create;
 
-window.onload = function() {
+window.onload = function () {
     setScreenTypeTexts();
     fillInputs();
 }
 
 function fillInputs() {
-    if(screenType === ScreenType.Edit) {
+    if (screenType === ScreenType.Edit) {
         fetch(`https://69adb822b50a169ec88017c7.mockapi.io/api/projects/${params.id}`, {
-        method: 'GET'
+            method: 'GET'
         })
-        .then(response => response.json())
-        .then(project => {
-            document.querySelector('#title').value = project.title;
-            document.querySelector('#total-cost').value = project.totalCost;
-            document.querySelector('#description').value = project.description;
-        })
+            .then(response => response.json())
+            .then(project => {
+                document.querySelector('#title').value = project.title;
+                document.querySelector('#total-cost').value = project.totalCost;
+                document.querySelector('#description').value = project.description;
+            })
     }
 }
 
 function setScreenTypeTexts() {
-    if(screenType == ScreenType.Create)
-    {
+    if (screenType == ScreenType.Create) {
         document.querySelector('#main-title').innerText = "Vamos cadastrar seu novo projeto!";
         document.querySelector('#action-button').innerText = "Cadastrar";
     }
-    
-    if(screenType == ScreenType.Edit)
-    {
+
+    if (screenType == ScreenType.Edit) {
         document.querySelector('#main-title').innerText = "Editar projeto";
         document.querySelector('#action-button').innerText = "Salvar";
     }
 }
 
 
-function createOrEdit()
-{
+function createOrEdit() {
     const payload = {
         title: document.querySelector('#title').value.trim(),
         totalCost: Number(document.querySelector('#total-cost').value.trim()),
         description: document.querySelector('#description').value.trim(),
-        idClient: 1
+        idClient: localStorage.getItem('idClient')
     };
-    
-    if(!isValid(payload)){
-        alert('Project is not valid!');
+
+    let result = validate(payload);
+
+    if (!result.isSuccess) {
+        alert(result.errorMessages.join('\n'));
         return;
     }
 
-    let queryParam = screenType === ScreenType.Edit ? `${params.id}` : ''; 
+    let queryParam = screenType === ScreenType.Edit ? `${params.id}` : '';
     let httpMethod = screenType === ScreenType.Edit ? 'PUT' : 'POST';
 
     fetch(`https://69adb822b50a169ec88017c7.mockapi.io/api/projects/${queryParam}`, {
@@ -66,26 +65,66 @@ function createOrEdit()
             'Content-Type': 'application/json'
         }
     })
-    .then(response => response.json())
-    .then(response => {
-        if(screenType === ScreenType.Edit) {
-            alert('Project edited successfully!');
-        } else {
-            alert('Project created successfully!');
-            window.location.href = "//localhost:5500/list.html"
-        }
-    })
-    .catch(error => {
-        alert('Internal Server Error');
-        console.log(error);
-    });
+        .then(response => response.json())
+        .then(response => {
+            if (screenType === ScreenType.Edit) {
+                alert('Project edited successfully!');
+            } else {
+                alert('Project created successfully!');
+                window.location.href = "//localhost:5500/list.html"
+            }
+        })
+        .catch(error => {
+            alert('Internal Server Error');
+            console.log(error);
+        });
 }
 
-function isValid(payload) {
-    const titleIsValid = payload.title !== '';
-    const descriptionIsValid = payload.description !== '';
-    const totalCostIsValid = Number.isFinite(payload.totalCost);
-    const idClientIsValid = Number.isFinite(payload.idClient);
+function validate(payload) {
 
-    return titleIsValid && totalCostIsValid && descriptionIsValid && idClientIsValid;
+    let validations = {
+        titleIsValid: payload.title !== '',
+        descriptionIsValid: payload.description !== '',
+        totalCostIsValid: numberIsValid(payload.totalCost),
+        idClientIsValid: numberIsValid(payload.idClient)
+    }
+
+    let result = {
+        isSuccess:
+            validations.titleIsValid &&
+            validations.totalCostIsValid &&
+            validations.descriptionIsValid &&
+            validations.idClientIsValid,
+        errorMessages: checkForErrorMessages(validations)
+    }
+
+    return result;
+}
+
+function numberIsValid(value) {
+    const numberValue = Number(value);
+
+    return (value !== '' && Number.isFinite(numberValue) && numberValue > 0);
+}
+
+function checkForErrorMessages(validations) {
+    let errorMessages = [];
+
+    if (!validations.titleIsValid) {
+        errorMessages = errorMessages.concat('Title is not valid!');
+    }
+
+    if (!validations.totalCostIsValid) {
+        errorMessages = errorMessages.concat('Total cost is not valid!');
+    }
+
+    if (!validations.descriptionIsValid) {
+        errorMessages = errorMessages.concat('Description is not valid!');
+    }
+
+    if (!validations.idClientIsValid) {
+        errorMessages = errorMessages.concat('Id client is not valid!');
+    }
+
+    return errorMessages;
 }
