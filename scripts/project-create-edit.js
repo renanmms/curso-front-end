c1965ad7c52dfe4a20c505febeef04f4176ac64c
Renan Martins <renanmartins1999@hotmail.com>
2026-03-24 19:56:38 -0300

    refactor: change screenType to const variable

--- scripts/project-create-edit.js
+++ scripts/project-create-edit.js
@@ -6,7 +6,7 @@ const ScreenType = {
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
 
-let screenType = params.id ? ScreenType.Edit : ScreenType.Create;
+const screenType = params.id ? ScreenType.Edit : ScreenType.Create;
 
 window.onload = function () {
     setScreenTypeTexts();
